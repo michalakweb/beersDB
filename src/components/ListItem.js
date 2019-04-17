c67155a0d454cccb245532050316ba9b5a851ba1
Mateusz Michalak <michalakweb@gmail.com>
2019-04-17 15:02:40 +0200

    list styled, logo added

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Row, Col, Button } from 'react-bootstrap';
 
 const ListItem = (props) => (
     <div>    
-        <p>Name: {props.database.name}</p>
-        <p>Abv: {props.database.abv ? props.database.abv : 'No information'}</p>
-        <p>Ibu: {props.database.ibu ? props.database.ibu : 'No information'}</p>
-        <Link to={{
-            pathname: `/beer/${props.database.id}`,
-            state: {
-              oneBeerData: props.database
-            }
-          }}>Details
-        </Link>
+        <Row>
+          <Col className='py-4' sm><h4>{props.database.name}</h4></Col>
+        </Row>
+        <Row className='text-center lead'>
+          <Col xs><p>Abv: {props.database.abv ? props.database.abv : '...'}</p></Col>
+          <Col xs><p>Ibu: {props.database.ibu ? props.database.ibu : '...'}</p></Col>
+          <Col xs>
+              <Link to={{
+                pathname: `/beer/${props.database.id}`,
+                state: {
+                  oneBeerData: props.database
+                }
+              }}><Button variant="outline-primary">Details</Button>
+              </Link>
+          </Col>
+          
+        
+        
+        
+        </Row>
     </div>
 )
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
+import logo from '../logo.svg';
 
 const Navigation = () => (
     <Navbar bg="dark" variant="dark">
-          <Navbar.Brand>
-            {' Beer DB'}
+          <Navbar.Brand className='d-flex'>
+          <img src={logo} alt="Logo" width="50" height="50" 
+            className="align-self-center"/>
+          <h3 className='align-self-center mb-0 pl-1'>Beer DB</h3>
           </Navbar.Brand>
     </Navbar>
 )
